79f19a7032ca2e767658e8103b1964ee9d5bfd50
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2023-05-16 23:03:36 -0300

    Fix sample

--- Sample/ViewKitSample/ViewKitSampleView.swift
+++ Sample/ViewKitSample/ViewKitSampleView.swift
@@ -1,13 +1,6 @@
 import ViewKit
 
 final class ViewKitSampleView: ProgrammaticView {
-    var notes: [String] = ["John", "Marie", "Carl"]
-    var showPassword = false {
-        didSet {
-            print("Show password: \(showPassword)")
-        }
-    }
-
     var body: UIView {
         VerticalStack {
 
@@ -28,50 +21,8 @@ final class ViewKitSampleView: ProgrammaticView {
                 .contentHuggingPriority(.required, for: .vertical)
                 .padding(15)
                 .round()
-                .maskToBounds()
-                .backgroundColor(.secondarySystemBackground)
                 .maxWidth()
-
-            UITextField()
-                .tag(1)
-                .placeholder("Password")
-                .autocapitalizationType(.none)
-                .autocorrectionType(.no)
-                .secureEntry()
-                .spellCheckingType(.no)
-                .rightViewMode(.always)
-                .clearsOnBeginEditing(false)
-                .contentHuggingPriority(.required, for: .vertical)
-                .rightView { textField in
-                    UIButton()
-                        .tintColor(.systemGray)
-                        .image(systemName: showPassword ? "eye" : "eye.slash")
-                        .action { button in
-                            self.showPassword.toggle()
-                            button.image(systemName: self.showPassword ? "eye" : "eye.slash")
-                            textField.isSecureTextEntry(!self.showPassword)
-                        }
-                }
-                .padding(15)
-                .round()
-                .maskToBounds()
-                .backgroundColor(.secondarySystemBackground)
-                .maxWidth()
-
-            UIButton("Sign in")
-                .backgroundColor(.label)
-                .titleColor(.systemBackground)
-                .titleColor(.systemGray, for: .disabled)
-                .disabled()
-                .height(50)
-                .round()
-                .maxWidth()
-
-            Filler()
         }
-        .maxWidth()
-        .padding()
-        .maxSize()
     }
 }
 
