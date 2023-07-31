65426abf6184983b471bcc016b8b35691af51a03
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2023-07-31 07:42:53 -0300

    Add stack view customization

--- Sources/ViewKit/UIStackView.swift
+++ Sources/ViewKit/UIStackView.swift
@@ -56,3 +56,35 @@ public class HorizontalStack: UIStackView {
         self.alignment = alignment
     }
 }
+
+public extension UIStackView {
+    func spacing(_ spacing: CGFloat) -> Self {
+        self.spacing = spacing
+        return self
+    }
+
+    func alignment(_ alignment: UIStackView.Alignment) -> Self {
+        self.alignment = alignment
+        return self
+    }
+
+    func distribution(_ distribution: UIStackView.Distribution) -> Self {
+        self.distribution = distribution
+        return self
+    }
+
+    func axis(_ axis: NSLayoutConstraint.Axis) -> Self {
+        self.axis = axis
+        return self
+    }
+
+    func isBaselineRelativeArrangement(_ isBaselineRelativeArrangement: Bool) -> Self {
+        self.isBaselineRelativeArrangement = isBaselineRelativeArrangement
+        return self
+    }
+
+    func isLayoutMarginsRelativeArrangement(_ isLayoutMarginsRelativeArrangement: Bool) -> Self {
+        self.isLayoutMarginsRelativeArrangement = isLayoutMarginsRelativeArrangement
+        return self
+    }
+}
